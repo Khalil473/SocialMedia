b7cf394ea9cfbf89ed28c4c61d8fbb511c314371
Khalil3Amer <khalil.3amer2000@gmail.com>
2022-06-10 14:10:34 +0300

    reformated and removed unnecessary code

--- user_auth/auth_backend.py
+++ user_auth/auth_backend.py
@@ -5,12 +5,15 @@ from django.contrib.auth.backends import ModelBackend
 class EmailBackend(ModelBackend):
     def authenticate(self, request, **kwargs):
         UserModel = get_user_model()
+        email = kwargs.get("email")
+        password = kwargs.get("password")
+        if email is None:
+            email = kwargs.get("username")
+        if not (email and password):
+            return None
         try:
-            email = kwargs.get("email", None)
-            if email is None:
-                email = kwargs.get("username", None)
             user = UserModel.objects.get(email=email)
-            if user.check_password(kwargs.get("password", None)):
+            if user.check_password(password):
                 return user
         except UserModel.DoesNotExist:
             return None
